77a2ae75d10dba4672e9e809db52801b91174e04
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-07-30 21:06:40 +0300

    fixed double json encoding

--- src/stagedata.py
+++ src/stagedata.py
@@ -3,17 +3,17 @@ import json
 def read_config_data(file_name):
     return read_data(file_name)
 
-def read_meta_data(file_name):
+def read_meta_data(file_name):    
     return read_data(file_name)['items']
 
 def read_data(file_name):
-    with open(file_name,'r') as json_file:
+    with open(file_name,'r', encoding='utf-8') as json_file:
         json_data = json.load(json_file)
     return json_data
 
 def write_data(items, file_name):
     data = {}
     data['items'] = items
-    json_data = json.dumps(data)
+    json_data = json.dumps(data, ensure_ascii=True, indent=4)
     with open(file_name, 'w', encoding='utf-8') as json_file:
-        json.dump(json_data, json_file, ensure_ascii=True, indent=4)
+        json_file.write(json_data)
