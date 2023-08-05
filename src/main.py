895f3eddedac064db5d202908e9afc7106b46fe4
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-08-05 22:46:12 +0300

    config_format parameter not passed anymore

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ else:
     print("This script needs exactly three arguments, aborting")
     exit()
 
-stage_data_utils = StageDataUtils(meta_data, 'json', config_data, 'json', output_data, 'csv')
+stage_data_utils = StageDataUtils(meta_data, 'json', config_data, output_data, 'csv')
 meta_data_json = stage_data_utils.read_meta_data()
 # meta_data_json = read_meta_data(meta_data)
 # print(meta_data_json)
