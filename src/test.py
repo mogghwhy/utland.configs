ae27bd556dc1494cf3bdf47cef569d4c1c0117f3
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-08-05 22:34:41 +0300

    using statedatautils class instead

--- src/test.py
+++ src/test.py
@@ -4,6 +4,7 @@ from skraper import scrape
 from stagedata import read_config_data
 from stagedata import read_meta_data
 from stagedata import write_data
+from stagedata import StageDataUtils
 
 
 if len(argv) == 4:
@@ -14,8 +15,13 @@ else:
     print("This script needs exactly three arguments, aborting")
     exit()
 
-meta_data_json = read_meta_data(meta_data)
+stage_data_utils = StageDataUtils(meta_data, 'json', config_data, 'json', output_data, 'csv')
+meta_data_json = stage_data_utils.read_meta_data()
+# meta_data_json = read_meta_data(meta_data)
+# print(meta_data_json)
 config_json = read_config_data(config_data)
-data = scrape(meta_data_json, config_json)
-write_data(data, output_data)
+# config_json2 = stage_data_utils.read_config(config_data)
+# print(config_json2)
+data = scrape(meta_data_json, config_json, stage_data_utils.write_data)
+#write_data(data, output_data)
 
