5ff7299ebc34c75c4a6bfdaf0eee30bbcf8b5a03
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-08-07 21:03:47 +0300

    code cleanup

--- src/stagedata.py
+++ src/stagedata.py
@@ -85,24 +85,3 @@ class StageDataUtils:
     
     def read_meta_data(self):        
         return self.read_data(self.input_file)['items']
-
-
-
-
-def read_config_data(file_name):
-    return read_data(file_name)
-
-def read_meta_data(file_name):    
-    return read_data(file_name)['items']
-
-def read_data(file_name):
-    with open(file_name,'r', encoding='utf-8') as json_file:
-        json_data = json.load(json_file)
-    return json_data
-
-def write_data(items, file_name):
-    data = {}
-    data['items'] = items
-    json_data = json.dumps(data, ensure_ascii=True, indent=4)
-    with open(file_name, 'w', encoding='utf-8') as json_file:
-        json_file.write(json_data)
